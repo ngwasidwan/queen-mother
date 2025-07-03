18690bcb70de66dbdfa754d7bc610768520ef51d
ngwasidwan <sidwanche@gmail.com>
2025-07-03 19:19:53 +0100

    fixed styles

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const initialData: Array<imgObj> = [
     image: "fried-plantain.jpg",
     containerStyle:
       " rounded-full overflow-hidden border-4 border-white w-34 h-34 relative",
-    imageStyle: "h-34 w-34 object-cover",
+    imageStyle: "h-32 w-32 object-cover",
   },
   {
     image: "potato.jpg",
@@ -28,13 +28,13 @@ const initialData: Array<imgObj> = [
 
     containerStyle:
       " rounded-full overflow-hidden border-4 border-white -ml-8 relative",
-    imageStyle: "h-34 w-34 object-cover",
+    imageStyle: "h-32 w-32 object-cover",
   },
   {
     image: "ndole.jpg",
     containerStyle:
       "rounded-full overflow-hidden border-4 border-white  absolute -bottom-20 z-20 left-1/2 transform -translate-x-1/2",
-    imageStyle: "h-34 w-34 object-cover",
+    imageStyle: "h-32 w-32 object-cover",
   },
 ];
 const services: Array<string> = [
@@ -42,13 +42,11 @@ const services: Array<string> = [
   "home delivery",
   "rental services",
   "catering services",
-  "private shef services",
   "meal preparation for events",
   "customize meal preparation",
 ];
 
 function App() {
-  // const [imageArr, setImageArr] = useState<string[]>(initImg);
   const [data, setData] = useState(initialData);
 
   const handleEditImage = (
@@ -74,26 +72,11 @@ function App() {
     }
   };
 
-  // const handleAddImage = (ev: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = ev.target.files?.[0];
-
-  //   if (file && file.type.startsWith("image/")) {
-  //     const reader = new FileReader();
-
-  //     reader.onload = (e) => {
-  //       const imgUrl = e.target?.result;
-
-  //       setImageArr((cur) => [...cur, imgUrl as string]);
-  //     };
-  //     reader.readAsDataURL(file);
-  //   }
-  // };
-
   return (
     <>
-      <div className="bg-black mx-auto relative w-full rounded xl overflow-hidden shadow-lg  text-white h-svh sm:h-auto">
-        <div className="p-6 mb-6 text-center">
-          <div className="absolute top-6 shadow-md shadow-orange-500 rounded-full overflow-hidden md:h-28 md:w-28 flex flex-col items-center justify-center h-20 w-20   text-xs md:text-base">
+      <div className="bg-black mx-auto relative w-full overflow-hidden   text-white h-svh sm:h-auto">
+        <div className="px-6 mt-4 mb-6 text-center">
+          <div className="absolute top-4 shadow-md shadow-orange-500 rounded-full overflow-hidden md:h-28 md:w-28 flex flex-col items-center justify-center h-20 w-20   text-xs md:text-base">
             <h1 className="font-semibold">Queen</h1>
             <h1 className=" font-semibold">Mother's </h1>
             <h1 className="">Cuisine</h1>
@@ -103,7 +86,7 @@ function App() {
               <GiSpoon />
             </div>
           </div>
-          <h2 className="text-white text-sm tracking-widest font-semibold">
+          <h2 className="text-white text-sm tracking-widest font-semibold ">
             TODAY's
           </h2>
           <h1 className="text-4xl font-bold text-orange-500 ">Special</h1>
@@ -141,25 +124,26 @@ function App() {
           </ul>
         </div>
 
-        <div className="flex  items-center flex-col gap-4 my-8">
+        <div className="flex  items-center flex-col gap-2 my-8">
           <button className=" bg-white text-black px-6 py-2 rounded-full font-bold mb-4">
             ORDER NOW
           </button>
 
           {/* <div> */}
-          <p className="flex items-center gap-2 font-semibold text-xl">
+          <p className="flex items-center gap-2 font-semibold text-lg">
             <IoLogoWhatsapp />
             <span>+ 237 651 103 724</span>
           </p>
           <p className="flex items-center gap-2">
             <IoLogoFacebook className="text-xl" />
-            <span>Queen Mother Restaurant</span>
+            <span className="font-semibold">Queen Mother Restaurant</span>
           </p>
           <p className="flex items-center gap-2">
             <LiaMapMarkerAltSolid className="text-xl" />
-            <span>Derriere Stade Cicam (Ange Raphael)</span>
+            <span className="font-semibold">
+              Derriere Stade Cicam (Ange Raphael)
+            </span>
           </p>
-          {/* </div> */}
         </div>
       </div>
     </>
